2ec9c18c26453022aa2327a94928e388863833b2
NicolasRodriguezNaro <nrodriguezortiz10@gmail.com>
2025-11-02 13:06:48 -0500

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// IMPORTANTE: usa el nombre EXACTO del repositorio, con mayúsculas/minúsculas tal cual
 export default defineConfig({
+  base: '/Calculadora-Modelos-de-Markov-y-Teoria-de-Colas/',
   plugins: [react()],
 })
